f5255c1bd9ea328a1c36dc8fc757442c90ee1c96
KUNAL CHOURE <chourekunal4@gmail.com>
2024-07-21 11:55:32 +0530

    added

--- src/generator.jsx
+++ src/generator.jsx
@@ -1,22 +1,42 @@
 import { useState } from "react"
-
+import QRCode from "react-qr-code";
+import { useEffect } from "react";
 export default function Generate(){
   let[val,setval]=useState("");
+  let[bgcolor,setcolor]=useState("fffff");
+  let[qrcode,setqrcode]=useState("");
+  let [size,setsize]=useState("");
+
   let handlechange=(event)=>{
     let value=event.target.value;
     console.log(value);
     setval(value);
   }
-  let handlesubmit=()=>{
+  useEffect(() => { 
+    setqrcode 
+ (`http://api.qrserver.com/v1/create-qr-code/?data=${val}!&size=${size}x${size}&bgcolor=${bgcolor}`); 
+  }, [val, size, bgcolor]); 
+  
+  let handlesubmit=(event)=>{
+event.preventDefault();
+setval(val);
+
     
   }
     return (
         <>
-         <form action="">
+        <div className="main">
+         <form action="" onSubmit={handlesubmit}>
             <input type="text" placeholder="ENTER TEXT" value={val} onChange={handlechange}/>
             <br /><br />
-          <button onSubmit={handlesubmit} >Submit</button>
+          
          </form>
+         <div className="code">
+       
+          <img src={qrcode} alt="" />
+        
+         </div>
+         </div>
         
         </>
     )
